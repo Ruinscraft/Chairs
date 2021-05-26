68a82c1458ccfe9c62a82784d81c00e0872d0cd4
Dexton Anderson <contact@dextonanderson.com>
2021-05-26 09:01:46 -0500

    Ignore not right click block

--- src/main/java/com/ruinscraft/chairs/PlayerListener.java
+++ src/main/java/com/ruinscraft/chairs/PlayerListener.java
@@ -5,6 +5,7 @@ import org.bukkit.block.data.type.Stairs;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -17,6 +18,10 @@ public class PlayerListener implements Listener {
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
         Block block = event.getClickedBlock();
+
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK) {
+            return;
+        }
 
         if (event.getHand() != EquipmentSlot.HAND) {
             return;
